2911ff21e6968b11a9bd102a172b7ebb76a206b5
EvelynGM14 <evelyn.gm14@hotmail.com>
2024-11-09 02:09:38 -0600

    Creación de login, registro y modificaciones

--- gatos/urls.py
+++ gatos/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('Inicio/', views.index, name='index'),
     path('TiposDeGatos/', views.tipos_de_gatos, name='TiposDeGatos'),
     path('Alimentacion/', views.alimentacion, name='Alimentacion'),
     path('Cuidado/', views.cuidado, name='Cuidado'),
--- gatos/views.py
+++ gatos/views.py
@@ -1,6 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormularioAdopcion
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')  # Asegúrate de que 'index' esté en urls.py
+        else:
+            print("Error en formulario de login:", form.errors)  # Ver errores en la consola
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('login')  # Redirige a la página principal
+        else:
+            print("Error en formulario de registro:", form.errors)  # Ver errores en la consola
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+# Vista para cerrar sesión
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Redirige a la página de login o a donde prefieras
+
+
+@login_required
 def index(request):
     return render(request, 'index.html')
 
@@ -20,9 +63,7 @@ def formulario_adopcion(request):
     if request.method == 'POST':
         form = FormularioAdopcion(request.POST)
         if form.is_valid():
-            # Aquí puedes guardar los datos en la base de datos o realizar otras acciones
-            # form.save()  # Si estuviera conectado a un modelo, por ejemplo.
-            return render(request, 'gracias.html')  # Página de gracias después de enviar el formulario
+            return render(request, 'gracias.html') 
     else:
         form = FormularioAdopcion()
 
